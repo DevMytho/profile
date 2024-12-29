f3b35f573abd956ad5654daa0895d8561e850177
Devvv <118110516+DevMytho@users.noreply.github.com>
2024-12-29 14:34:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // typed js
 const typed = new Typed(".typing", {
-  strings: ["Python Lover ", "Freelancer ", "Web Developer "],
+  strings: ["Freelancer ", "Web Developer "],
   loop: true,
   typeSpeed: 80,
   backSpeed: 40,
